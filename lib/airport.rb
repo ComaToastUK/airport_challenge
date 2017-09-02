89cc72e0b4f0cb9e2acafdab54294185e867fbe6
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-09-02 22:30:05 +0100

    added planes count method for confirming planes in airport, fail and confirmation message for plane_takeoff

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,18 +9,29 @@ class Airport
 
   def plane_land(plane)
     @planes << plane
+    puts "#{plane} has landed"
+    planes_count
   end
 
   def plane_takeoff(plane)
     fail "No planes currently in the airport" if empty?
     @planes.delete(plane)
     puts "#{plane} has left the building"
+    planes_count
   end
-
-end
 
 private
 
-def empty?
-  @planes.empty?
+  def empty?
+    @planes.empty?
+  end
+
+  def planes_count
+    if @planes.count == 1
+      puts "There is currently #{@planes.count} plane in the airport"
+    else
+      puts "There are currently #{@planes.count} planes in the airport"
+    end
+  end
+
 end
